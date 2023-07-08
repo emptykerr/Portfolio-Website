bbceff3ca121e6e8bc6fad22257db222b8912cd6
emptykerr <104610868+emptykerr@users.noreply.github.com>
2023-07-09 00:33:59 +1200

    Added bugfixes, dark-mode/light-mode, screen compatibility

--- js/script.js
+++ js/script.js
@@ -8,6 +8,8 @@ let navbar = document.querySelector('.navbar');
 const darkMode = document.querySelector('.switch');
 let switchContainer = document.querySelector('.switch-container');
 let switchIcon = document.querySelector('.switch i');
+const home = document.getElementById('home');
+const currentBackground = home.style.background;
 //state
 const theme = localStorage.getItem('theme');
 
@@ -23,12 +25,19 @@ menuIcon.onclick = () => {
 darkMode.onclick = () => {
     document.body.classList.toggle('dark-mode');
     localStorage.setItem('theme', document.body.classList.contains('dark-mode'));
+    if (document.body.classList.contains('dark-mode')) {
+        home.style.backgroundImage = "url('../images/background-light.png')";
+    } else {
+        home.style.backgroundImage = "url('../images/background.png')";
+    }
 }
+
 
 switchContainer.onclick = () => {
     switchContainer.classList.toggle('active');
     switchIcon.classList.toggle('bx-toggle-left');
-    switchIcon.classList.toggle('bx-toggle-right');
+    switchIcon.classList.toggle('bxs-toggle-right');
+ 
 }
 
 menuIcon.onclick = () => {
@@ -63,6 +72,13 @@ window.onscroll = () => {
         else{
             sec.classList.remove('show-animate');
         }
+
+          // Check if user has reached the bottom of the page
+          if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+            // Add show-animate class to the contact section
+            document.querySelector('.contact').classList.add('show-animate');
+            document.querySelector('.footer').classList.add('show-animate');
+        }
     })
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
@@ -75,3 +91,19 @@ window.onscroll = () => {
     let footer = document.querySelector('footer');
     footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }
+
+const textPrompts = ["Software Developer", "Designer", "Creative Thinker", "Web Designer", "Teacher", "Programmer"]; // Array of text prompts
+
+const h3Element = document.querySelector('.text-animate h3');
+
+function rotateText() {
+  // Get a random text prompt from the array
+  const randomIndex = Math.floor(Math.random() * textPrompts.length);
+  const newText = textPrompts[randomIndex];
+  
+  // Change the text content of the <h3> element
+  h3Element.textContent = newText;
+}
+
+// Add event listener to the animationend event of the text animation
+h3Element.addEventListener('animationend', rotateText);
