50b762d49e76876a55e5656988ff74c3d6127b8c
emptykerr <104610868+emptykerr@users.noreply.github.com>
2023-07-20 17:56:58 +1200

    Bugfixes, Adjusted image size, removed pages
    Fixed bug between padding on the contact page, removed designer and programmer cover page sections. Made landing page image smaller

--- js/script.js
+++ js/script.js
@@ -167,32 +167,61 @@ function checkVisible(elm) {
 
 
   //Slideshow for programmer section
-
   let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
+  let slideInterval;
+  
+  showSlides(slideIndex);
+  startSlideInterval();
+  
+  // Function to start the slideshow interval
+  function startSlideInterval() {
+    slideInterval = setInterval(() => plusSlides(1), 4000);
+  }
+  
+  // Function to pause the slideshow interval
+  function pauseSlideInterval() {
+    clearInterval(slideInterval);
+    setTimeout(() => {
+      startSlideInterval();
+    }, 15000);
+  }
+  
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides((slideIndex += n));
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides((slideIndex = n));
+  }
+  
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active-dot", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active-dot";
+  }
+  
+  // Add click event listeners to dots
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].addEventListener("click", () => {
+      currentSlide(i + 1);
+      pauseSlideInterval();
+    });
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active-dot", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active-dot";
-} 
+  
